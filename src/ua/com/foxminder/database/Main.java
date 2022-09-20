b5fb8018aa8419d821c0994910cfd3ee665d7b33
Stanislav <stas.buka@gmail.com>
2022-09-20 03:02:08 +0300

    SQL Work jdbc Driver done

--- src/ua/com/foxminder/database/Main.java
+++ src/ua/com/foxminder/database/Main.java
@@ -6,17 +6,20 @@ import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
+
         DBWorker worker = new DBWorker();
 
-        String query = "select * from books";
+        String query = "select * from books"; //строка для формирования запросов
         try {
             Statement statement = worker.getConnection().createStatement();
             ResultSet resultSet = statement.executeQuery(query);
 
-            while (resultSet.next()){
+            while (resultSet.next()){ //перебор всех строк таблицы
+
                 Book book = new Book();
-                book.setId(resultSet.getInt(1));
-                book.setBookName(resultSet.getString(2));
+
+                book.setId(resultSet.getInt(1)); //доступ по имени поля "id"
+                book.setBookName(resultSet.getString(2)); //доступ по имени поля "name"
 
                 System.out.println(book);
             }
